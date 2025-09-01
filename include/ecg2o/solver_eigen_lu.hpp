ad6d509ab67cd83832044d516bb5c3257f4204a0
Anas-Abdelkarim <54266723+Anas-Abdelkarim@users.noreply.github.com>
2025-09-01 11:56:40 +0200

    update LU solver

--- include/ecg2o/solver_eigen_lu.hpp
+++ include/ecg2o/solver_eigen_lu.hpp
@@ -1,3 +1,32 @@
+/*
+Copyright (c) 2023, University of Luxembourg
+All rights reserved.
+
+Redistributions and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+*/
+
 #ifndef BIPM_G2O_SOLVER_EIGEN_LU_HPP
 #define BIPM_G2O_SOLVER_EIGEN_LU_HPP
 
@@ -8,7 +37,7 @@
 #include "g2o/core/optimization_algorithm_levenberg.h"
 #include "g2o/core/sparse_optimizer.h"
 #include "g2o/stuff/logger.h"
-#include "linear_solver_eigen_lu.h"  // Updated header
+#include "linear_solver_eigen_lu.h" // Updated header
 
 using namespace std;
 
@@ -17,20 +46,20 @@ namespace g2o {
 namespace {
 template <int p, int l, bool blockorder>
 std::unique_ptr<BlockSolverBase> AllocateSolverLU() {
-  G2O_DEBUG(
-      "Using EigenSparseLU poseDim {} landMarkDim {} blockordering {}", p,
-      l, blockorder);
+  G2O_DEBUG("Using EigenSparseLU poseDim {} landMarkDim {} blockordering {}", p,
+            l, blockorder);
   auto linearSolver = std::make_unique<
       LinearSolverEigenLU<typename BlockSolverPL<p, l>::PoseMatrixType>>();
   linearSolver->setBlockOrdering(blockorder);
   return std::make_unique<BlockSolverPL<p, l>>(std::move(linearSolver));
 }
-}  // namespace
+} // namespace
 
 /**
  * Helper function for allocating LU-based solvers
  */
-static OptimizationAlgorithm* createSolverLU(const std::string& fullSolverName) {
+static OptimizationAlgorithm *
+createSolverLU(const std::string &fullSolverName) {
   static const std::map<std::string,
                         std::function<std::unique_ptr<BlockSolverBase>()>>
       solver_factories{
@@ -45,7 +74,8 @@ static OptimizationAlgorithm* createSolverLU(const std::string& fullSolverName) 
 
   string solverName = fullSolverName.substr(3);
   auto solverf = solver_factories.find(solverName);
-  if (solverf == solver_factories.end()) return nullptr;
+  if (solverf == solver_factories.end())
+    return nullptr;
 
   string methodName = fullSolverName.substr(0, 2);
 
@@ -61,10 +91,10 @@ static OptimizationAlgorithm* createSolverLU(const std::string& fullSolverName) 
 }
 
 class EigenLU_SolverCreator : public AbstractOptimizationAlgorithmCreator {
- public:
-  explicit EigenLU_SolverCreator(const OptimizationAlgorithmProperty& p)
+public:
+  explicit EigenLU_SolverCreator(const OptimizationAlgorithmProperty &p)
       : AbstractOptimizationAlgorithmCreator(p) {}
-  virtual OptimizationAlgorithm* construct() {
+  virtual OptimizationAlgorithm *construct() {
     return createSolverLU(property().name);
   }
 };
@@ -72,28 +102,66 @@ class EigenLU_SolverCreator : public AbstractOptimizationAlgorithmCreator {
 // Register LU-based solvers
 G2O_REGISTER_OPTIMIZATION_LIBRARY(eigen_partial_lu);
 
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(gn_var_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("gn_var_partial_lu", "Gauss-Newton: LU solver using Eigen's SparseLU methods (variable blocksize)", "Eigen", false, Eigen::Dynamic, Eigen::Dynamic)));
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(gn_fix3_2_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("gn_fix3_2_partial_lu", "Gauss-Newton: LU solver using Eigen's SparseLU methods (fixed blocksize)", "Eigen", true, 3, 2)));
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(gn_fix6_3_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("gn_fix6_3_partial_lu", "Gauss-Newton: LU solver using Eigen's SparseLU methods (fixed blocksize)", "Eigen", true, 6, 3)));
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(gn_fix7_3_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("gn_fix7_3_partial_lu", "Gauss-Newton: LU solver using Eigen's SparseLU methods (fixed blocksize)", "Eigen", true, 7, 3)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    gn_var_partial_lu, new EigenLU_SolverCreator(OptimizationAlgorithmProperty(
+                           "gn_var_partial_lu",
+                           "Gauss-Newton: LU solver using Eigen's SparseLU "
+                           "methods (variable blocksize)",
+                           "Eigen", false, Eigen::Dynamic, Eigen::Dynamic)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    gn_fix3_2_partial_lu,
+    new EigenLU_SolverCreator(
+        OptimizationAlgorithmProperty("gn_fix3_2_partial_lu",
+                                      "Gauss-Newton: LU solver using Eigen's "
+                                      "SparseLU methods (fixed blocksize)",
+                                      "Eigen", true, 3, 2)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    gn_fix6_3_partial_lu,
+    new EigenLU_SolverCreator(
+        OptimizationAlgorithmProperty("gn_fix6_3_partial_lu",
+                                      "Gauss-Newton: LU solver using Eigen's "
+                                      "SparseLU methods (fixed blocksize)",
+                                      "Eigen", true, 6, 3)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    gn_fix7_3_partial_lu,
+    new EigenLU_SolverCreator(
+        OptimizationAlgorithmProperty("gn_fix7_3_partial_lu",
+                                      "Gauss-Newton: LU solver using Eigen's "
+                                      "SparseLU methods (fixed blocksize)",
+                                      "Eigen", true, 7, 3)));
 
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(lm_var_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("lm_var_partial_lu", "Levenberg: LU solver using Eigen's SparseLU methods (variable blocksize)", "Eigen", false, Eigen::Dynamic, Eigen::Dynamic)));
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(lm_fix3_2_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("lm_fix3_2_partial_lu", "Levenberg: LU solver using Eigen's SparseLU methods (fixed blocksize)", "Eigen", true, 3, 2)));
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(lm_fix6_3_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("lm_fix6_3_partial_lu", "Levenberg: LU solver using Eigen's SparseLU methods (fixed blocksize)", "Eigen", true, 6, 3)));
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(lm_fix7_3_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("lm_fix7_3_partial_lu", "Levenberg: LU solver using Eigen's SparseLU methods (fixed blocksize)", "Eigen", true, 7, 3)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    lm_var_partial_lu, new EigenLU_SolverCreator(OptimizationAlgorithmProperty(
+                           "lm_var_partial_lu",
+                           "Levenberg: LU solver using Eigen's SparseLU "
+                           "methods (variable blocksize)",
+                           "Eigen", false, Eigen::Dynamic, Eigen::Dynamic)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    lm_fix3_2_partial_lu,
+    new EigenLU_SolverCreator(OptimizationAlgorithmProperty(
+        "lm_fix3_2_partial_lu",
+        "Levenberg: LU solver using Eigen's SparseLU methods (fixed blocksize)",
+        "Eigen", true, 3, 2)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    lm_fix6_3_partial_lu,
+    new EigenLU_SolverCreator(OptimizationAlgorithmProperty(
+        "lm_fix6_3_partial_lu",
+        "Levenberg: LU solver using Eigen's SparseLU methods (fixed blocksize)",
+        "Eigen", true, 6, 3)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    lm_fix7_3_partial_lu,
+    new EigenLU_SolverCreator(OptimizationAlgorithmProperty(
+        "lm_fix7_3_partial_lu",
+        "Levenberg: LU solver using Eigen's SparseLU methods (fixed blocksize)",
+        "Eigen", true, 7, 3)));
 
-G2O_REGISTER_OPTIMIZATION_ALGORITHM(dl_var_partial_lu, new EigenLU_SolverCreator(
-    OptimizationAlgorithmProperty("dl_var_partial_lu", "Dogleg: LU solver using Eigen's SparseLU methods (variable blocksize)", "Eigen", false, Eigen::Dynamic, Eigen::Dynamic)));
+G2O_REGISTER_OPTIMIZATION_ALGORITHM(
+    dl_var_partial_lu,
+    new EigenLU_SolverCreator(OptimizationAlgorithmProperty(
+        "dl_var_partial_lu",
+        "Dogleg: LU solver using Eigen's SparseLU methods (variable blocksize)",
+        "Eigen", false, Eigen::Dynamic, Eigen::Dynamic)));
 
-}  // namespace g2o
+} // namespace g2o
 
-
-#endif  // BIPM_G2O_SOLVER_EIGEN_LU_HPP
+#endif // BIPM_G2O_SOLVER_EIGEN_LU_HPP
